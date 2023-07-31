db3b94a7256b96f4536b83fad8f65a7902950219
DESKTOP-18588J2\USER <skes09605@gmail.com>
2023-07-31 21:25:04 +0800

    匯入汙染跟測站資料

---
+++ competition_NTU.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 21:16:38 2023
+
+@author: USER
+"""
+import pandas as pd
+df_pollution = pd.read_csv(r'D:\程式競賽\pollution_data_1.csv',encoding = 'big5')
+df_station=pd.read_csv(r'D:\程式競賽\station_ifo.csv')
