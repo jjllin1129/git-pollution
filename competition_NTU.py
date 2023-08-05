81d5188c7abb76dfda2a8d1b9eb33c5317230a44
DESKTOP-18588J2\USER <skes09605@gmail.com>
2023-08-05 23:16:32 +0800

    讀取PM2.5的資料(但還是serise)

--- competition_NTU.py
+++ competition_NTU.py
@@ -6,4 +6,9 @@ Created on Mon Jul 31 21:16:38 2023
 """
 import pandas as pd
 df_pollution = pd.read_csv(r'D:\程式競賽\pollution_data_1.csv',encoding = 'big5')
-df_station=pd.read_csv(r'D:\程式競賽\station_ifo.csv')
+len_pollution=len(df_pollution)
+data_pm25=[]
+for i in range(len_pollution):
+    if df_pollution.loc[i,"ItemName"]=="細懸浮微粒":
+        data_pm25.append(df_pollution.loc[i,:])
+# df_station=pd.read_csv(r'D:\程式競賽\station_ifo.csv')
