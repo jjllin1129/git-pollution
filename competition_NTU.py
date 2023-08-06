fe81c5c1802c6fe6f1153e48982a5fc9b31ee132
DESKTOP-18588J2\USER <skes09605@gmail.com>
2023-08-06 14:48:53 +0800

    把PM2.5資料取出，然後把NaN部分移除

--- competition_NTU.py
+++ competition_NTU.py
@@ -5,10 +5,18 @@ Created on Mon Jul 31 21:16:38 2023
 @author: USER
 """
 import pandas as pd
+"""
+處理pollution資料
+"""
+#讀取資料
 df_pollution = pd.read_csv(r'D:\程式競賽\pollution_data_1.csv',encoding = 'big5')
+#取PM2.5資料
 len_pollution=len(df_pollution)
 data_pm25=[]
 for i in range(len_pollution):
     if df_pollution.loc[i,"ItemName"]=="細懸浮微粒":
         data_pm25.append(df_pollution.loc[i,:])
+df=pd.DataFrame(data_pm25)
+#把NaN部分移除
+data_pm25=df.dropna(axis=0)
 # df_station=pd.read_csv(r'D:\程式競賽\station_ifo.csv')
