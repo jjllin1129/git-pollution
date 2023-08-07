f9bc7737838995daf737055d798106ea9b7b30f9
DESKTOP-18588J2\USER <skes09605@gmail.com>
2023-08-07 21:25:56 +0800

    把每一列的總和算出來並且將資料按照日期1/1~12/31排列

--- competition_NTU.py
+++ competition_NTU.py
@@ -8,9 +8,9 @@ import pandas as pd
 """
 處理pollution資料
 """
-#讀取資料
+#%%讀取資料
 df_pollution = pd.read_csv(r'D:\程式競賽\pollution_data_1.csv',encoding = 'big5')
-#取PM2.5資料
+#%%取PM2.5資料
 data_pm25=[]
 for i in range(len(df_pollution)):
     if df_pollution.loc[i,"ItemName"]=="細懸浮微粒":
@@ -24,6 +24,14 @@ for j in range((len(df))):
     if "x" not in df.iloc[j,:].values:
         nox.append(df.iloc[j,:])
 df_nox=pd.DataFrame(nox)
+#%%計算每一行的總和
+#先將計算範圍的數值轉換為整數
+num = df_nox.columns[8:]  # 轉換從第8列開始的數據為int
+df_nox[num] = df_nox[num].astype(int)
+sum_row = df_nox[num].sum(axis=1)
+df_nox["總和"]=sum_row
+df_nox=df_nox.sort_values(by='MonitorDate', ascending=True)#排序由1/1~12/31
 # #存檔(csv)
-df_nox.to_csv(r'D:\程式競賽\data_pm2_5.csv', index=False, encoding='big5')
-# df_station=pd.read_csv(r'D:\程式競賽\station_ifo.csv')
+# df_nox.to_csv(r'D:\程式競賽\data_pm2_5.csv', index=False, encoding='big5')
+
+
