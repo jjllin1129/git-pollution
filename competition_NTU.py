14af1fb0dfd17dcb05897e19a58dd2bb252b1981
DESKTOP-18588J2\USER <skes09605@gmail.com>
2023-08-07 17:04:51 +0800

    把x部分移除，順便減少ccde

--- competition_NTU.py
+++ competition_NTU.py
@@ -11,16 +11,19 @@ import pandas as pd
 #讀取資料
 df_pollution = pd.read_csv(r'D:\程式競賽\pollution_data_1.csv',encoding = 'big5')
 #取PM2.5資料
-len_pollution=len(df_pollution)
 data_pm25=[]
-for i in range(len_pollution):
+for i in range(len(df_pollution)):
     if df_pollution.loc[i,"ItemName"]=="細懸浮微粒":
         data_pm25.append(df_pollution.loc[i,:])
 df=pd.DataFrame(data_pm25)
 #把NaN部分移除
-data_pm25=df.dropna(axis=0)
-#存檔
-df_1=pd.DataFrame(data_pm25)
-df_1.to_excel(r'D:\程式競賽\data_pm2_5.xlsx', index=False)
-df_1.to_csv(r'D:\程式競賽\data_pm2_5.csv', index=False, encoding='big5')
+df=df.dropna(axis=0)
+#把x部分移除
+nox=[]
+for j in range((len(df))):
+    if "x" not in df.iloc[j,:].values:
+        nox.append(df.iloc[j,:])
+df_nox=pd.DataFrame(nox)
+# #存檔(csv)
+df_nox.to_csv(r'D:\程式競賽\data_pm2_5.csv', index=False, encoding='big5')
 # df_station=pd.read_csv(r'D:\程式競賽\station_ifo.csv')
